5b7da5a14ee8f99f9a71a9f035c9f00f2942163b
OriJC <40464032+OriJC@users.noreply.github.com>
2019-12-20 04:04:27 +0800

    sleep

--- project/js/Function.js
+++ project/js/Function.js
@@ -1,24 +1,27 @@
 $(()=>{
-    var posConv = { };
+    var postable = { };
     for(let m=0; m<16; m++)
     {   
         $("#mainwindow").append("<div class='puzzle' id='Puz"+m+"'>"+(m+1)+"</div>");
         $("#Puz"+m).addClass("block");
         var row=parseInt(m / 4);
         var col = m%4;
-        posConv[m] = { row:row, col:col };
+        postable[m] = { row:row, col:col };
     }
     $("#Puz15").empty();
 
-
+    var count=0;
     $(".puzzle").click(function(){
         var cells = $("#mainwindow div");
         var i = cells.index(this);
         var empty = getPostion(i);
+        console.log(array);
         while (empty.length > 0) {
             var j = empty.pop();
             if (cells.eq(j).attr("id") == "Puz15") 
             {
+                count++;
+                $("#countno").html("移動次數:"+count);
                 if (i > j) { var k = j; j = i; i = k; }
                 var ahead = cells.eq(i);
                 var behind = cells.eq(j);
@@ -33,21 +36,26 @@ $(()=>{
                 break;
             }       
         } 
+        var array=[];
+        console.log(postable);
     })
 
     $('#btn').on('click',()=>{
-        for(let m=0;m<500;m++){
+        for(let m=0;m<1000;m++){
             var block = $("#mainwindow div");
-            blocks.eq(toMove[ 
-                parseInt(rand(1,9999) * toMove.length) 
+            var move = getPostion(block.index($("#Puz15")[0]));
+            block.eq(move[ 
+                parseInt(Math.random() * move.length) 
             ]).click();
         }
+        count=0;
+        $("#countno").html("移動次數:"+count);
     })
 
     
     var getPostion=(i)=>{
         var pool = []
-        var row = posConv[i].row, col = posConv[i].col;
+        var row = postable[i].row, col = postable[i].col;
         if (row > 0)
             pool.push((row - 1) *  4 + col);
         if (row < 4)
@@ -58,12 +66,5 @@ $(()=>{
             pool.push( i + 1);
         return pool;
     }
-
-    var rand = (start, end) => {
-        var r
-        n = end - start + 1 
-        r = Math.random() * n 
-        r = Math.floor(r) 
-        r += start 
-    }
+    
 })
